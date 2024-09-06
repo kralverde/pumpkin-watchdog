2f79e874d9f59a77a822db8e1c67117d9c87505a
kralverde <github@email.kralverde.dev>
2024-09-05 21:45:31 -0400

    print test

--- watchdog.py
+++ watchdog.py
@@ -135,6 +135,7 @@ async def handle_webhook(queue: asyncio.Queue[str], request: web.Request):
     raw_data = await request.text()
     converted_data = urllib.parse.unquote(raw_data)
     json_data = json.loads(converted_data[len("payload=") :])
+    print(json_data["repository"])
     print(f'commit detected on {json_data["ref"]}')
     if json_data["ref"] == "refs/heads/master":
         await queue.put(json_data["after"])
